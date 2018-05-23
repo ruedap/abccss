c6aeeb41670ceac41bb0e56f118cb86307f0c490
ruedap <ruedap@ruedap.com>
2018-05-24 02:04:29 +0900

    chore(storybook): Add screenshot setting to stories

--- .storybook/stories/Components/Checkmark.stories.jsx
+++ .storybook/stories/Components/Checkmark.stories.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withScreenshot } from "storybook-chrome-screenshot";
 
 storiesOf("Components/Atoms/Checkmark", module)
+  .addDecorator(withScreenshot())
   .add("default", () => <span className="c-Checkmark" />)
   .add("2rem", () => (
     <span className="c-Checkmark" style={{ fontSize: "2rem" }} />
