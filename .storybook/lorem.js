72c97677da0f7d6687a47b58181c2a1bdebf1a2b
ruedap <ruedap@ruedap.com>
2018-05-26 14:43:26 +0900

    feat(scss): Add transition utilities
    https://stackoverflow.com/questions/28623446/hover-effect-expand-bottom-border

---
+++ .storybook/lorem.js
@@ -0,0 +1,7 @@
+const lorem = {};
+
+lorem.ja =
+  "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。";
+lorem.en = "Grumpy wizards make toxic brew for the evil Queen and Jack.";
+
+export default lorem;
