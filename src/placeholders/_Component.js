ecfdf80c5032645b8284f1ec3be84925e514e5a3
ruedap <ruedap@ruedap.com>
2018-06-05 00:40:19 +0900

    feat(placeholders): Remove unnecessary example code

--- src/placeholders/_Component.js
+++ src/placeholders/_Component.js
@@ -6,41 +6,12 @@ module.exports = name => `// ===================================================
   // Abstractions
   // ===========================================================================
 
-  $_BlockName: &;
-  $_fontSize: a-px2rem(16px);
-  $_elementName-fontSize: a-px2rem(16px);
-  $_elementName--elementModifierName-fontSize: a-px2rem(16px);
-  $_--blockModifierName-bgColor: $a-color-primary;
-  $_--blockModifierName-elementName-bgColor: $a-color-primary;
-  content: "ここはこのファイル内のみで使用する変数・関数・ミックスインを定義します。プレフィックスとしてアンダースコアを付与してください。";
-
   // Block
   // ===========================================================================
-
-  content: "ここはBEMのBlockに相当するスタイルを定義します。";
 
   // Elements
   // ===========================================================================
 
-  &-elementName {
-    content: "ここはBEMのElementに相当するスタイルを定義します。";
-
-    &--elementModifierName {
-      content: "ここはBEMのElementのModifierに相当するスタイルを定義します。";
-    }
-  }
-
   // Modifiers
   // ===========================================================================
-
-  &--blockModifierName {
-    content: "ここはBEMのBlockのModifierに相当するスタイルを定義します。";
-
-    #{$_BlockName} {
-      &-elementName {
-        content: "ここはBEMのBlockのModifierのElementに相当するスタイルを定義します。";
-      }
-    }
-  }
-}
 `;
