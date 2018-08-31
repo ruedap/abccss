eceae2e604ead58f0ed8e9659f2ca158d7351f49
ruedap <ruedap@ruedap.com>
2018-08-31 19:37:39 +0900

    fix(scss): Remove unnecessary import declaration

--- src/templates/rails/app/javascript/packs/application.js
+++
@@ -1,9 +0,0 @@
-import "../stylesheets/basics.scss";
-import "../stylesheets/components.scss";
-import "../stylesheets/decorations.scss";
-import "../stylesheets/extras.scss";
-require.context(
-  "../images",
-  true,
-  /\.(jpg|jpeg|png|gif|tiff|ico|svg|eot|otf|ttf|woff|woff2)$/i
-);
