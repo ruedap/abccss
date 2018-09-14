f13713f28067e00f6c4d5e8c8ec751e50bfd88af
ruedap <ruedap@ruedap.com>
2018-09-14 14:09:25 +0900

    feat(scss): Improve font family

--- .storybook/lorem.js
+++ .storybook/lorem.js
@@ -1,8 +1,8 @@
 const lorem = {};
-
+const emojis = "😀😸🤡💃💅💥⚡✏⚠⤴⤵";
 lorem.ja =
   "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。";
 lorem.en = "Grumpy wizards make toxic brew for the evil Queen and Jack.";
-lorem.all = `${lorem.ja}${lorem.en}`;
+lorem.all = `${lorem.ja}${lorem.en} ${emojis}`;
 
 export default lorem;
